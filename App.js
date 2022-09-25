95bbc8a11b7f89c7207c26293d37495138d58a8b
abdullah <98920860+cyber-abdullahh@users.noreply.github.com>
2022-09-26 04:42:29 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+
+
+function App() {
+  return (
+    <div className="App">
+          <ExpenseItem />
+          
+    </div>
+  );
+}
+
+export default App;
